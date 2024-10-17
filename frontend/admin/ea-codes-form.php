747151bf5d73a7e5779fcbccb28fd580daf07624
daniyalbutt <34391174+daniyalbutt@users.noreply.github.com>
2024-10-18 02:57:55 +0500

    Update ea-codes-form.php

--- frontend/admin/ea-codes-form.php
+++ frontend/admin/ea-codes-form.php
@@ -4,7 +4,7 @@
         <div class="page-title">
             <div class="row">
                 <div class="col-12">
-                    <h4>EA Codes Form</h4>
+                    <h4>EA Code List</h4>
                 </div>
             </div>
         </div>
@@ -44,7 +44,7 @@
             <div class="col-sm-12">
                 <div class="card">
                     <div class="card-header pb-0 card-no-border">
-                        <h4>EA Codes Form List</h4>
+                        <h4>EA Codes List</h4>
                     </div>
                     <div class="card-body">
                         <div class="table-responsive custom-scrollbar">
