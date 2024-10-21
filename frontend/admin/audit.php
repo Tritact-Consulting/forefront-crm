f529e8c55e82d7cea92490fa36d4d7b07b6a4459
daniyalbutt <daniyalbutt785@gmail.com>
2024-10-21 22:29:58 +0500

    In-Progress Update

--- frontend/admin/audit.php
+++ frontend/admin/audit.php
@@ -17,7 +17,7 @@
                         <div class="col-md-6">
                             <ul class="nav nav-tabs border-tab" id="top-tab" role="tablist">
                                 <li class="nav-item"><a class="nav-link active" id="top-home-tab" data-bs-toggle="tab" href="#top-home" role="tab" aria-controls="top-home" aria-selected="true"><i data-feather="target"></i>All</a></li>
-                                <li class="nav-item"><a class="nav-link" id="profile-top-tab" data-bs-toggle="tab" href="#top-profile" role="tab" aria-controls="top-profile" aria-selected="false"><i data-feather="info"></i>Progress</a></li>
+                                <li class="nav-item"><a class="nav-link" id="profile-top-tab" data-bs-toggle="tab" href="#top-profile" role="tab" aria-controls="top-profile" aria-selected="false"><i data-feather="info"></i>In-Progress</a></li>
                                 <li class="nav-item"><a class="nav-link" id="contact-top-tab" data-bs-toggle="tab" href="#top-contact" role="tab" aria-controls="top-contact" aria-selected="false"><i data-feather="check-circle"></i>Complete</a></li>
                             </ul>
                         </div>
