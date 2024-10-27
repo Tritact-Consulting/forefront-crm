ac87f68d8ac4b14eb0fc243fb4c8c8da4483e1fe
daniyalbutt <34391174+daniyalbutt@users.noreply.github.com>
2024-10-27 05:16:10 +0500

    Create Allocation list

---
+++ frontend/admin/allocation-list.php
@@ -0,0 +1,91 @@
+<?php include 'include/header.php'; ?>
+<div class="page-body">
+    <div class="container-fluid">
+        <div class="page-title">
+            <div class="row">
+                <div class="col-12">
+                    <h4>Allocation List</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+            <!-- Zero Configuration  Starts-->
+            <div class="col-sm-12">
+                <div class="card">
+                    <div class="card-header pb-0 card-no-border">
+                        <h4>Allocation List</h4>
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive custom-scrollbar">
+                            <table class="display" id="basic-1">
+                                <thead>
+                                    <tr>
+                                        <th>Client</th>
+                                        <th>Certificate</th>
+                                        <th>ISO Standards</th>
+                                        <th>Date</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Client 01</td>
+                                        <td>Certificate 01</td>
+                                        <td>ISO 14001</td>
+                                        <td>21-04-22</td>
+                                        <td>
+                                            <ul class="action">
+                                                <li class="edit"> <a href="#"><i class="fa fa-edit"></i></a></li>
+                                                <li class="delete"><a href="#"><i class="fa fa-trash-o"></i></a></li>
+                                            </ul>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Client 02</td>
+                                        <td>Certificate 02</td>
+                                        <td>ISO 45001</td>
+                                        <td>04-01-23</td>
+                                        <td>
+                                            <ul class="action">
+                                                <li class="edit"> <a href="#"><i class="fa fa-edit"></i></a></li>
+                                                <li class="delete"><a href="#"><i class="fa fa-trash-o"></i></a></li>
+                                            </ul>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Client 03</td>
+                                        <td>Certificate 03</td>
+                                        <td>ISO 50001</td>
+                                        <td>12-02-22</td>
+                                        <td>
+                                            <ul class="action">
+                                                <li class="edit"> <a href="#"><i class="fa fa-edit"></i></a></li>
+                                                <li class="delete"><a href="#"><i class="fa fa-trash-o"></i></a></li>
+                                            </ul>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Client 04</td>
+                                        <td>Certificate 04</td>
+                                        <td>ISO 9001</td>
+                                        <td>27-01-22</td>
+                                        <td>
+                                            <ul class="action">
+                                                <li class="edit"> <a href="#"><i class="fa fa-edit"></i></a></li>
+                                                <li class="delete"><a href="#"><i class="fa fa-trash-o"></i></a></li>
+                                            </ul>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include 'include/footer.php'; ?>
